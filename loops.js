766d81d8a33da6aada0aa78b2f664c4e8a909059
William Doody <wdoody@yahoo.com>
2018-03-20 21:29:04 -0600

    Commit 9 - Created loops.js file.

---
+++ loops.js
@@ -0,0 +1,43 @@
+// Inside the loops.js file, create a loop that logs the numbers from 0-99 (ascending)
+
+for (let i=1; i <= 99; i++) {
+    console.log(i);
+}
+
+// Inside the loops.js file, create a loop that logs the numbers from 99-0 (descending)
+
+for (let i=100; i >= 0; i--) {
+    console.log(i);
+  }
+
+// Inside the loops.js file, create a loop that logs the EVEN numbers from 0-98 (ascending)
+
+for (let i=0; i <= 98; i+=2) {
+    console.log(i);
+}
+
+// Inside the loops.js file, create a loop that logs the EVEN numbers from 98-0 (descending)
+
+for (let i=98; i >= 0; i-=2) {
+    console.log(i);
+  }
+
+// Inside the loops.js file, create a loop that logs the numbers from 49-72 (ascending)
+
+for (let i=49; i <= 72; i++) {
+    console.log(i);
+}
+
+// Inside the loops.js file, create a loop that logs the numbers from 81-26 (descending)
+
+for (let i=81; i >= 26; i--) {
+    console.log(i);
+  }
+
+// Inside the loops.js file, create a loop that logs the numbers from 3-90 that are multiples of 3 (ascending)
+
+/*this one has me beat
+for (let i=49; i <= 72; i++) {
+    console.log(i*3);
+}
+*/
